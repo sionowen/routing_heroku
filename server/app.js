6ff6ad44fcee154d9732dd179fc6dd6dd354a2b3
sionowen <sionowen34@gmail.com>
2016-05-13 14:32:28 -0500

    changes

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
